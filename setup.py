86d1d311e4034487e311a847bc20f41ed9f8a5cb
jheo <jeewon.heo@cern.ch>
2023-07-19 22:21:29 +0900

    fix import

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+import setuptools
 
 
-setup(
+setuptools.setup(
     name='dilepton-saja',
     version='0.0.1',
 
     url='https://github.com/JeewonHeo/SAJA-Dilepton/saja',
     author='Jeewon Heo',
     author_email='jeewon.heo@cern.ch',
-    packages=setup.find_packages()
+    packages=setuptools.find_packages()
 
 )
