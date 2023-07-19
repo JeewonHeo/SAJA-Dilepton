5071c9ae3b271b4dc775abc5c2f9b5770377f707
jheo <jeewon.heo@cern.ch>
2023-07-19 22:19:59 +0900

    add package

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     name='dilepton-saja',
     version='0.0.1',
 
-    url='https://github.com/JeewonHeo/SAJA-Dilepton/',
+    url='https://github.com/JeewonHeo/SAJA-Dilepton/saja',
     author='Jeewon Heo',
     author_email='jeewon.heo@cern.ch',
+    packages=setup.find_packages()
 
 )
