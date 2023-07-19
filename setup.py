349a4935d693ced09f3307e8dcfbe94b8925b32f
jheo <jeewon.heo@cern.ch>
2023-07-19 22:11:29 +0900

    for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='dilepton-saja',
+    version='dev',
+
+    url='https://github.com/JeewonHeo/SAJA-Dilepton/',
+    author='Jeewon Heo',
+    author_email='jeewon.heo@cern.ch',
+
+)
